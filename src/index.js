d3289b548999ffe0351295b8ad9470be6927116f
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-20 20:35:55 -0300

    cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log("Server startered");
+app.use(express.json());
+
+const costumers = [];
+
+app.post("/account", (req, resp) => {
+  const id = uuidV4();
+  const { cpf, name } = req.body;
+
+  costumers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return resp.status(201).send();
+});
+
+app.listen(3000, () => {
+  console.log("🚀️ Server startered");
 });
