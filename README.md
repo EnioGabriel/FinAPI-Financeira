d23f7750b3bdc0fad7c0a9146cadab305aad405b
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-08-23 15:29:59 -0300

    Validando CPF existente

--- README.md
+++ README.md
@@ -126,7 +126,7 @@ $ cd FinAPI-Financeira
 
 ## Regras de negócio
 
-- [ ] Não deve ser possivel cadastrar uma conta com CPF já existente
+- [x] Não deve ser possivel cadastrar uma conta com CPF já existente
 - [ ] Não deve ser possivel fazer depósito em uma conta não existente
 - [ ] Não deve ser possivel buscar extrato em uma conta não existente
 - [ ] Não deve ser possivel fazer saque em ua conta não existente
--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
+// Importando libs
 const express = require("express");
 const { v4: uuidV4 } = require("uuid");
 
+// Inicializando o express
 const app = express();
 
+// Habilita o uso de objetos .json
 app.use(express.json());
 
+// Array para armazenar dados do cliente
 const costumers = [];
 
+// Cadastro do cliente
 app.post("/account", (req, resp) => {
-  const id = uuidV4();
+  // Desestruturando e pegando as info (JSON) do corpo da req
   const { cpf, name } = req.body;
 
+  // Varrendo array e verificando se contem cpf repetido
+  // some => retorna verdadeiro ou falso dependendo da condiçao passada para ele
+  const costumerAlreadyExist = costumers.some((costumer) => {
+    return costumer.cpf === cpf;
+  });
+
+  // Retornando erro caso o CPF já esteja cadastrado
+  if (costumerAlreadyExist) {
+    return resp.status(400).json({ error: "esse CPF já está em uso!" });
+  }
+
+  // Inserindo dados no array
   costumers.push({
-    id,
+    id: uuidV4(),
     cpf,
     name,
     statement: [],
   });
 
-  return resp.status(201).send();
+  // Enviando a respoosta para o client-server
+  return resp.status(201).send("Cadastro criado!");
 });
 
-app.listen(3000, () => {
-  console.log("🚀️ Server startered");
+// Atribuindo porta para conexao e escutando
+app.listen(3333, () => {
+  console.log("🚀️ Server started");
 });
